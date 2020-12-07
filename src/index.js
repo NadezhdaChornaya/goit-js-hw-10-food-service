e23ee7bfbaaccc57e16bd6da20f14cf929870b9c
Nadezhda Chornaya <nadezhdachornaja@gmail.com>
2020-12-07 12:28:46 +0200

    references are collected in an object

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './styles.css';
 import data from "./data/menu.json"
 import cards from "./templates/cards.hbs"
-// import 'material-desing-icons/iconfont/material-icons.css';
-
 
 
 const Theme = {
@@ -10,80 +8,37 @@ const Theme = {
     DARK: 'dark-theme',
 };
 
-const checkboxRef = document.getElementById("theme-switch-toggle");
-const bodyRef = document.querySelector("body");
+const refs = {
+    checkboxRef: document.getElementById("theme-switch-toggle"),
+    bodyRef: document.querySelector("body"),
+    ulRef: document.querySelector(".js-menu"),
+}
 
 const onDarkTheme = () => {
-    bodyRef.classList.contains("light-theme") && bodyRef.classList.remove("light-theme");
+    refs.bodyRef.classList.contains("light-theme") && refs.bodyRef.classList.remove("light-theme");
 
-    bodyRef.classList.add("dark-theme");
+    refs.bodyRef.classList.add("dark-theme");
 
 }
 
 const onLightTheme = () => {
-    bodyRef.classList.contains("dark-theme") && bodyRef.classList.remove("dark-theme");
+    refs.bodyRef.classList.contains("dark-theme") && refs.bodyRef.classList.remove("dark-theme");
 
-    bodyRef.classList.add("light-theme");
+    refs.bodyRef.classList.add("light-theme");
 
 }
 
 // приводим из string в boolean из localStorage
 if (localStorage.getItem("Theme")) {
     const checkedBoolean = JSON.parse(localStorage.getItem("Theme"));
-    checkboxRef.checked = checkedBoolean;
-    checkboxRef.checked ? onDarkTheme() : onLightTheme();
+    refs.checkboxRef.checked = checkedBoolean;
+    refs.checkboxRef.checked ? onDarkTheme() : onLightTheme();
 }
 const checkboxState = (e) => {
     // приводим в string из boolean для localStorage
     localStorage.setItem("Theme", JSON.stringify(e.target.checked))
     e.target.checked ? onDarkTheme() : onLightTheme();
 }
-checkboxRef.addEventListener("change", checkboxState);
+refs.checkboxRef.addEventListener("change", checkboxState);
 
-// ==========================================================
-
-const ulRef = document.querySelector(".js-menu");
-
-// const createMarkup = (dataArray) => {
-//     return dataArray.reduce((acc, { image, description, name, price, ingredients }) => {
-//         acc += `
-//         <li class="menu__item">
-//   <article class="card">
-//     <img
-//       src=${image}
-//       alt=${description}
-//       class="card__image"
-//     />
-//     <div class="card__content">
-//       <h2 class="card__name">${name}</h2>
-//       <p class="card__price">
-//         <i class="material-icons"> monetization_on </i>
-//         ${price} кредитов
-//       </p>
-
-//       <p class="card__descr">
-//         ${description}
-//       </p>
-
-//       <ul class="tag-list">
-//       ${ingredients.map(ingredient =>
-//             (`<li class="tag-list__item">${ingredient}</li>`)).join('')
-//             }
-//       </ul>
-//     </div>
-
-//     <button class="card__button button">
-//       <i class="material-icons button__icon"> shopping_cart </i>
-//       В корзину
-//     </button>
-//   </article>
-// </li>
-//     `
-
-//         return acc;
-//     }, "")
-// };
-
-// ulRef.innerHTML = `<ul>${createMarkup(data)}</ul>`
-
-ulRef.innerHTML = cards(data);
+refs.ulRef.innerHTML = cards(data);
